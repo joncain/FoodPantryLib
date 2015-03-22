5c6bd63d6ac7386239dcd9b9561566c768e15c26
joncain <jon@solutionx.com>
2015-03-22 12:22:24 -0600

    Filter deleted recipients from the search results

--- Search.cs
+++ Search.cs
@@ -12,7 +12,7 @@ namespace FoodPantryLib
         public DataSet DoSearch(string searchText)
         {
             ClassDb db = new ClassDb();
-            db.Exec(string.Format("SELECT R.rowid RecipientID, R.First || ' ' || R.Last Name,  COUNT(D.rowid) +1 HouseholdCount, strftime('%m/%d/%Y', R.DOB) BirthDate, R.Phone, R.Address1, R.City FROM Recipients R LEFT JOIN Recipients D ON (R.rowid = D.ParentRecipientID AND D.Deleted IS NULL) WHERE (R.First LIKE '%{0}%' OR R.Last LIKE '%{0}%' OR R.Phone LIKE '%{0}%' )AND R.ParentRecipientID IS NULL GROUP BY R.rowid, Name, BirthDate, R.Phone, R.Address1, R.City", searchText.Replace(" ", string.Empty)));
+            db.Exec(string.Format("SELECT R.rowid RecipientID, R.First || ' ' || R.Last Name,  COUNT(D.rowid) +1 HouseholdCount, strftime('%m/%d/%Y', R.DOB) BirthDate, R.Phone, R.Address1, R.City FROM Recipients R LEFT JOIN Recipients D ON (R.rowid = D.ParentRecipientID AND D.Deleted IS NULL) WHERE R.Deleted IS NULL AND (R.First LIKE '%{0}%' OR R.Last LIKE '%{0}%' OR R.Phone LIKE '%{0}%' )AND R.ParentRecipientID IS NULL GROUP BY R.rowid, Name, BirthDate, R.Phone, R.Address1, R.City", searchText.Replace(" ", string.Empty)));
             return db.Results;
         }
 
